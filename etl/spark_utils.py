4d9be2009ba6c41481a6d9cdad0867208e9724aa
ak <khaxar@gmail.com>
2025-10-11 20:08:13 +0330

    delete spark_utils.py

--- etl/spark_utils.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-def build_spark(app_name="ecom-etl"): 
-    return (SparkSession.builder 
-            .appName(app_name)
-            .config("spark.sql.shuffle.partitions", "8") 
-            .getOrCreate())
